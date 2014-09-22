3b929c525c7ea58915debde79926964768591e02
Stephen Bain <stephenb@aloompa.com>
2014-09-22 11:27:59 -0500

    state/problem setup

--- src/samegame/BoardState.java
+++ src/samegame/BoardState.java
@@ -1,24 +1,100 @@
 package samegame;
 
-public class BoardState extends jaima.search.State{
-	
-	
+import jaima.search.SearchAction;
+import jaima.search.State;
+
+import java.util.Map;
+
+public class BoardState extends jaima.search.State {
+
+	public int[] colors;
 
 	public BoardState(String fromString) {
 		super(fromString);
-		// TODO Auto-generated constructor stub
+		
+		colors = new int[fromString.length()];
+		
+		for(int i = 0; i < fromString.length(); i++){
+			colors[i] = fromString.charAt(i);
+		}
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		String s = "";
+		for(int color : colors){
+			s += color;
+		}
+		
+		return s;
 	}
-
+	
 	@Override
 	public jaima.search.State clone() {
-		// TODO Auto-generated method stub
-		return null;
+		return new BoardState(toString());
 	}
+	
+	public boolean isEmpty(){
+		for(int color : colors){
+			if(color != 0){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public Map<SearchAction, State> successors(){
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @return true if colors[n] has adjacent boxes of the same color
+	 */
+	public boolean isRemovable(int row, int column){
+		int color = getColor(row, column);
+		
+		// color of -1 means it is an edge piece
+		int leftColor = column > 0 ? getColor(row, column - 1) : -1;
+		int rightColor = column < Math.sqrt(colors.length) ? getColor(row, column + 1) : -1;
+		int upColor = row > 0 ? getColor(row - 1, column) : -1;
+		int downColor = row < Math.sqrt(colors.length) ? getColor(row + 1, column) : -1;
+		
+		return color == leftColor || color == rightColor || color == upColor || color == downColor;
+	}
+	
+	/**
+	 * 
+	 * @param row
+	 * @param column
+	 * @return an integer value that corresponds to the color at specified row and column
+	 */
+	public int getColor(int row, int column){
+		int i = (int) (column + (Math.sqrt(colors.length) * row));
+		
+		return colors[i];
+	}
+	
+	/**
+	 * @param n
+	 * @return row of colors[n]
+	 */
+	public int getRow(int n){
+		return (int) (n / Math.sqrt(colors.length));
+	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @return column of colors[n]
+	 */
+	public int getColumn(int n){
+		return (int) (n % Math.sqrt(colors.length));
+	}
+
+	
 
 }
--- src/samegame/Problem.java
+++ src/samegame/Problem.java
@@ -3,46 +3,47 @@ package samegame;
 import jaima.search.SearchAction;
 import jaima.search.State;
 
-import java.util.ArrayList;
 import java.util.Map;
-import java.util.TreeMap;
 
-public class Problem extends jaima.search.Problem{
-	
-	private String start;
-	private String goal = "000000000";
-	private ArrayList<Remove> moves;
-	private Map<String, BoardState> states;
-	
+public class Problem extends jaima.search.Problem {
+
+	private BoardState board;
+	private BoardState goal;
+
 	public Problem(String start) {
-		this.start = start;
-		moves = new ArrayList<Remove>();
-		states = new TreeMap<String, BoardState>();
+		board = new BoardState(start);
+	}
+
+	public void setGoal(String s) {
+		goal = new BoardState(s);
+	}
+
+	public void setBoard(String s) {
+		board = new BoardState(s);
 	}
 
 	@Override
 	public State initial() {
-		return states.get(start);
+		return (State) board;
+	}
+
+	public State goal() {
+		return goal;
 	}
 
 	@Override
 	public boolean isGoal(State s) {
-		return s.toString().equals(goal);
+		if (goal == null) {
+			BoardState state = (BoardState) s;
+			return state.isEmpty();
+		}
+
+		return s.equals(goal);
 	}
 
 	@Override
 	public Map<SearchAction, State> successors(State state) {
-		TreeMap<SearchAction, State> possible = new TreeMap<SearchAction, State>();
-		
-		for(Remove move : moves){
-			if(move.from.equals(state)){
-				SearchAction action = move;
-                State newState = move.to;
-                possible.put(action, newState);
-			}
-		}
-		
-		return possible;
+		return ((BoardState) state).successors();
 	}
 
 }
