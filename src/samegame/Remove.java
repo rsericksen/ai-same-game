5c9ce804e067e70a0cbbda46ec7cfa020b0a4fff
Stephen Bain <stephenb@aloompa.com>
2014-09-24 11:29:08 -0500

    moved remove logic to action class

--- src/samegame/Remove.java
+++ src/samegame/Remove.java
@@ -28,7 +28,47 @@ public class Remove extends jaima.search.SearchAction {
 	}
 
 	public BoardState getTo() {
-		return from.remove(group);
+		BoardState newState = from.clone();
+
+		// replace array locations in group with empty character
+		for (int i = 0; i < group.locations.size(); i++) {
+			newState.colors[group.locations.get(i)] = BoardState.EMPTY;
+		}
+
+		// if square is empty and square above it is not, swap the two
+		int row;
+		int column;
+		char up;
+		char left;
+		for (int j = 0; j < Math.sqrt(newState.colors.length); j++) {
+			for (int i = 0; i < newState.colors.length; i++) {
+				if (newState.colors[i] == BoardState.EMPTY) {
+					row = newState.getRow(i);
+					column = newState.getColumn(i);
+
+					if (row > 0) {
+						up = newState.getColor(row - 1, column);
+						if (up != BoardState.EMPTY && up != BoardState.EDGE) {
+							int upLocation = newState.getLocation(row - 1,
+									column);
+							newState.colors[upLocation] = BoardState.EMPTY;
+							newState.colors[i] = up;
+						}
+					}
+					
+					if(column > 0) {
+						left = newState.getColor(row, column - 1);
+						if(left != BoardState.EDGE && left != BoardState.EMPTY){
+							int leftLocation = newState.getLocation(row, column - 1);
+							newState.colors[leftLocation] = BoardState.EMPTY;
+							newState.colors[i] = left;
+						}
+					}
+				}
+			}
+		}
+
+		return newState;
 	}
 
 }
